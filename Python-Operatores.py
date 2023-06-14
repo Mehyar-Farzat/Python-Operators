fac009eedf509057c5737be0caca58031cd9ca48
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-06-15 01:46:39 +0200

    Create python operators file

---
+++ Python-Operatores.py
@@ -0,0 +1,82 @@
+Arithmetic Operatores
+
+x = 10
+y = 5
+print(x + y)
+15
+print(x - y)
+5
+print(x * y)
+50
+print(x / y)
+2.0
+print(x // y)
+2
+print(x % y)
+0
+print(x ** 2)
+100
+x = 10
+print(x + 2)
+12
+-----------------------
+
+Assignment Operators
+x = 10
+
+x += 2
+print(x)
+12
+
+x -= 4
+print(x)
+6
+
+x *= 3
+print(x)
+30
+
+x /= 2
+print(x)
+5.0
+
+x //= 2
+print(x)
+5
+
+x **= 2
+print(x)
+100
+-------------------------
+
+Comparison Operators
+x = 5
+y = 5
+
+print(x == y)
+True
+
+print(x != y)
+False
+
+print(x >= y)
+False
+
+print(x <= y)
+False
+--------------------------
+
+Logical Operators
+
+x = 6
+
+print(x > 5 and x < 8)
+True
+
+print(x > 3 or x < 2)
+True
+
+print(not(x > 2 and x < 10)
+False
+
+
